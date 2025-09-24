593bc2fe8e0aff51b6a8b9400fd6ba15def04ae4
Hritikraj8804 <hritikraj8804@gmail.com>
2025-09-24 21:29:48 +0530

    Spring App

---
+++ CSEJENKINS/src/main/java/com/example/demo/CsejenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsejenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsejenkinsApplication.class, args);
+	}
+
+}
